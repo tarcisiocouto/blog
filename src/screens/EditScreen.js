1d0d9f0bf7e437ce56aac61426ce5024abb178e2
Tarcísio Couto <tarcisio.pereira@caruaru.ifpe.edu.br>
2019-10-25 10:45:27 -0300

    criando um component BlogFormPost

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,52 +1,19 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
     const { state } = useContext(Context);
     const blogPost = state.find(blogPost => blogPost.id === navigation.getParam('id'));
 
-    const [title, setTitle] = useState(blogPost.title);
-    const [content, setContent] = useState(blogPost.content);
-
     return (
-        <View>
-            <Text style={styles.label}>Edit Title:</Text>
-            <TextInput style={styles.input}
-                value={title}
-                onChangeText={(text) => setTitle(text)}
-            />
-            <Text style={styles.label}>Edit Content:</Text>
-            <TextInput style={styles.input}
-                value={content}
-                onChangeText={(text) => setContent(text)}
-            />
-            <Button 
-                title="Edit Blog Post"
-                onPress={ () => {
-                    addBlogPost(title, content, () => {
-                        navigation.navigate('Index');
-                    });
-                }}
-            />
-        </View>
+        <BlogPostForm></BlogPostForm>
     );
 };
 
 const styles = StyleSheet.create({
-    input: {
-        fontSize: 18,
-        borderBottomWidth: 1,
-        borderBottomColor: 'black',
-        padding: 5,
-        margin: 5
-    },
-
-    label: {
-        fontSize: 20,
-        marginBottom: 5,
-        marginLeft: 5
-    }
+    
 });
 
 export default EditScreen;
